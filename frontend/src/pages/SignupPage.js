57e28054a9d88ad4aa3e52e77ac27b1afb8242ad
ShreyansSST <tharu3218@gmail.com>
2024-08-13 12:34:32 +0545

    changes

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -104,126 +104,126 @@ const SignupPage = () => {
   };
 
   return (
-    // <div className="bg-container bg-[url('./assets/images/light-bg.jpg')] dark:bg-[url('./assets/images/dark-bg.jpg')] bg-contain bg-no-repeat min-h-[100vh] flex bg-light-bg dark:bg-dark-bg">
-    //   <div className="w-full flex flex-col items-center sm:flex-row-reverse">
-    //     <RingAndCardsDesign />
+    <div className="bg-container bg-[url('./assets/images/light-bg.jpg')] dark:bg-[url('./assets/images/dark-bg.jpg')] bg-contain bg-no-repeat min-h-[100vh] flex bg-light-bg dark:bg-dark-bg">
+      <div className="w-full flex flex-col items-center sm:flex-row-reverse">
+        <RingAndCardsDesign />
 
-    //     <AuthCard>
-    //       <Link to="/">
-    //         <button className="border dark:border-white p-1 rounded-[3px] sm:hidden">
-    //           <FaAngleLeft className="text-xl text-black dark:text-white" />
-    //         </button>
-    //       </Link>
-    //       <div className="w-full flex flex-col gap-8">
-    //         <div>
-    //           <h1 className="dark:text-white">Sign up</h1>
+        <AuthCard>
+          <Link to="/">
+            <button className="border dark:border-white p-1 rounded-[3px] sm:hidden">
+              <FaAngleLeft className="text-xl text-black dark:text-white" />
+            </button>
+          </Link>
+          <div className="w-full flex flex-col gap-8">
+            <div>
+              <h1 className="dark:text-white">Sign up</h1>
 
-    //           <p className="text-dark-slate">
-    //             Enter your personal information to create a new account
-    //           </p>
-    //         </div>
+              <p className="text-dark-slate">
+                Enter your personal information to create a new account
+              </p>
+            </div>
 
-    //         <form className="flex flex-col gap-4">
-    //           {error && <span className="text-pale-red">{error}</span>}
+            <form className="flex flex-col gap-4">
+              {error && <span className="text-pale-red">{error}</span>}
 
-    //           {message && <span className="text-pale-green">{message}</span>}
+              {message && <span className="text-pale-green">{message}</span>}
 
-    //           <TextFieldWithLabel
-    //             label="Full Name"
-    //             type="text"
-    //             placeholder="Enter your full name"
-    //             icon={HiUserCircle}
-    //             value={fullname}
-    //             onChange={(e) => setFullname(e.target.value)}
-    //           />
+              <TextFieldWithLabel
+                label="Full Name"
+                type="text"
+                placeholder="Enter your full name"
+                icon={HiUserCircle}
+                value={fullname}
+                onChange={(e) => setFullname(e.target.value)}
+              />
 
-    //           <TextFieldWithLabel
-    //             label="Username"
-    //             type="text"
-    //             placeholder="Enter your username"
-    //             icon={HiUser}
-    //             value={username}
-    //             onChange={(e) => setUsername(e.target.value)}
-    //           />
+              <TextFieldWithLabel
+                label="Username"
+                type="text"
+                placeholder="Enter your username"
+                icon={HiUser}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
 
-    //           <TextFieldWithLabel
-    //             label="Email"
-    //             type="email"
-    //             placeholder="Enter your email"
-    //             icon={HiMail}
-    //             value={email}
-    //             onChange={(e) => setEmail(e.target.value)}
-    //           />
+              <TextFieldWithLabel
+                label="Email"
+                type="email"
+                placeholder="Enter your email"
+                icon={HiMail}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
 
-    //           <PasswordFieldWithLabel
-    //             label="Password"
-    //             placeholder="Enter your password"
-    //             value={password}
-    //             onChange={handlePasswordChange}
-    //           />
+              <PasswordFieldWithLabel
+                label="Password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
 
-    //           {password && (
-    //             <div className="text-sm text-gray-500">
-    //               Password Strength:{" "}
-    //               <span
-    //                 className={`text-${getPasswordStrengthColor(
-    //                   passwordStrength
-    //                 )}`}
-    //               >
-    //                 {getPasswordStrengthLabel(passwordStrength)}
-    //               </span>
-    //             </div>
-    //           )}
+              {password && (
+                <div className="text-sm text-gray-500">
+                  Password Strength:{" "}
+                  <span
+                    className={`text-${getPasswordStrengthColor(
+                      passwordStrength
+                    )}`}
+                  >
+                    {getPasswordStrengthLabel(passwordStrength)}
+                  </span>
+                </div>
+              )}
 
-    //           <div className="flex flex-col gap-2 mb-2">
-    //             <div className="flex gap-2">
-    //               <input
-    //                 type="checkbox"
-    //                 name="terms-of-service"
-    //                 id="terms-of-service"
-    //                 className="custom-checkbox relative top-1 dark:border-white"
-    //               />
-    //               <label
-    //                 htmlFor="terms-of-service"
-    //                 className=" dark:text-white"
-    //               >
-    //                 I have read and agree to the Terms of Service
-    //               </label>
-    //             </div>
-    //           </div>
+              <div className="flex flex-col gap-2 mb-2">
+                <div className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    name="terms-of-service"
+                    id="terms-of-service"
+                    className="custom-checkbox relative top-1 dark:border-white"
+                  />
+                  <label
+                    htmlFor="terms-of-service"
+                    className=" dark:text-white"
+                  >
+                    I have read and agree to the Terms of Service
+                  </label>
+                </div>
+              </div>
 
-    //           <PrimaryButton
-    //             btnLabel="Create Account"
-    //             isLoading={isLoading}
-    //             onClick={handleSignup}
-    //           />
+              <PrimaryButton
+                btnLabel="Create Account"
+                isLoading={isLoading}
+                onClick={handleSignup}
+              />
 
-    //           <div className="flex items-center gap-4 px-[10%] py-2">
-    //             <hr className="flex-1 border dark:border-white" />
-    //             <p className="font-semibold dark:text-white">or</p>
-    //             <hr className="flex-1 border dark:border-white" />
-    //           </div>
+              <div className="flex items-center gap-4 px-[10%] py-2">
+                <hr className="flex-1 border dark:border-white" />
+                <p className="font-semibold dark:text-white">or</p>
+                <hr className="flex-1 border dark:border-white" />
+              </div>
 
-    //           <Link to="/">
-    //             <button className="w-full flex items-center justify-center gap-2 bg-none border border-black dark:border-white rounded-[3px] py-3 px-5 sm:px-8">
-    //               <FaUser className="text-xl text-black dark:text-white" />
-    //               <p className="font-semibold dark:text-white">
-    //                 Continue as Guest
-    //               </p>
-    //             </button>
-    //           </Link>
-    //         </form>
+              <Link to="/">
+                <button className="w-full flex items-center justify-center gap-2 bg-none border border-black dark:border-white rounded-[3px] py-3 px-5 sm:px-8">
+                  <FaUser className="text-xl text-black dark:text-white" />
+                  <p className="font-semibold dark:text-white">
+                    Continue as Guest
+                  </p>
+                </button>
+              </Link>
+            </form>
 
-    //         <div className="hidden sm:flex flex-col items-center gap-1 lg:flex-row lg:gap-2 2xl:mt-[10%]">
-    //           <p className="dark:text-white">Already registered?</p>
-    //           <Link to="/signin" className="font-bold dark:text-white">
-    //             Sign in to your account
-    //           </Link>
-    //         </div>
-    //       </div>
-    //     </AuthCard>
-    //   </div>
-    // </div>
+            <div className="hidden sm:flex flex-col items-center gap-1 lg:flex-row lg:gap-2 2xl:mt-[10%]">
+              <p className="dark:text-white">Already registered?</p>
+              <Link to="/signin" className="font-bold dark:text-white">
+                Sign in to your account
+              </Link>
+            </div>
+          </div>
+        </AuthCard>
+      </div>
+    </div>
   );
 };
 
